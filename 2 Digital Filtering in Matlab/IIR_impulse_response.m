f928532d4e63b767d03d666064269ce955fdc204
ZhannaKlimanova <zklimanova@yahoo.com>
2023-01-17 22:34:49 -0500

    Adding frequency and phase response plot.

--- 2 Digital Filtering in Matlab/IIR_impulse_response.m
+++ 2 Digital Filtering in Matlab/IIR_impulse_response.m
@@ -5,3 +5,4 @@ a = [1 -2.3 -0.8]; b = [0.3 1.5 -0.3];
 impulse = [1; zeros(49,1)];
 y = filter(b, a,impulse);
 figure; stem(0:49, y); title('Impulse Response of a Digital Filter'); xlabel('Normalized Frequency'); ylabel('Magnitude (dB)');
+figure; freqz(y);
