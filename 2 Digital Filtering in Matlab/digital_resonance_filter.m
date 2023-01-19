a22b8389e0fb101e9ea8f462ada48a828689e271
ZhannaKlimanova <zklimanova@yahoo.com>
2023-01-18 23:15:48 -0500

    mean calculation added

--- 2 Digital Filtering in Matlab/digital_resonance_filter.m
+++ 2 Digital Filtering in Matlab/digital_resonance_filter.m
@@ -21,8 +21,12 @@ figure;
 plot(W, abs(H)); title('Frequency Response of a Digital Filter'); xlabel('Normalized Frequency'); ylabel('Magnitude (dB)');
 
 % Filtering a noise signal
-noise = rand(1, Fs); % todo
-disp(['average of noise signal: ', mean(noise)]);
+% noise = rand(1, Fs/2); 
+% noisy_noise = [noise, -noise]; % random generated noise with a mean of 0
+% disp(mean(noisy_noise));
+noise = rand(1, Fs);
+centered_noise = noise - mean(noise);
+
 noise_filter = filter(b, a, noise);
 figure;
 spectrogram(noise_filter);
