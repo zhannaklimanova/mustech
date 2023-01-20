18cb9549f8f597e987169da3337b7bdad671496e
ZhannaKlimanova <zklimanova@yahoo.com>
2023-01-19 20:29:16 -0500

    Updating digital filters.

--- 2 Digital Filtering in Matlab/IIR_impulse_response.m
+++ 2 Digital Filtering in Matlab/IIR_impulse_response.m
@@ -1,8 +1,8 @@
-% Difference equaation => y[n] = 0.3x[n] + 1.5x[n-1] - 0.3x[n-2] - 2.3y[n-1] - 0.8y[n-2] 
+% Difference equaation => y[n] = 0.3x[n] + 1.5x[n-1] + (-0.3)x[n-2] - (-2.3)y[n-1] - (-0.8)y[n-2] 
 
 % IIR filter equation
 a = [1 -2.3 -0.8]; b = [0.3 1.5 -0.3];
 impulse = [1; zeros(49,1)];
-y = filter(b, a,impulse);
-figure; stem(0:49, y); title('Impulse Response of a Digital Filter'); xlabel('Normalized Frequency'); ylabel('Magnitude (dB)');
+y = filter(b, a, impulse);
+figure; stem(0:49, y); title('Impulse Response of an IIR Filter'); xlabel('Time'); ylabel('Magnitude (dB)');
 figure; freqz(y);
--- 2 Digital Filtering in Matlab/digital_resonance_filter.m
+++ 2 Digital Filtering in Matlab/digital_resonance_filter.m
@@ -4,7 +4,7 @@ Fs = 44100; % Hz
 T = 1/Fs;
 r = 0.99;
 
-a1 = -2 * r * cos(2*pi*Fs*T);
+a1 = -2*r*cos(2*pi*center_frequency*T);
 a2 = r.^2;
 b0 = (1-r.^2)/2;
 b1 = 0;
@@ -15,19 +15,16 @@ a = [1 a1 a2];
 
 impulse = [1; zeros(49,1)];
 y = filter(b, a, impulse);
+
 % Frequnecy Response
 [H, W] = freqz(b, a);
 figure; 
-plot(W, abs(H)); title('Frequency Response of a Digital Filter'); xlabel('Normalized Frequency'); ylabel('Magnitude (dB)');
+plot(W, abs(H)); title('Frequency Response of a Digital Resonance Filter'); xlabel('Normalized Frequency'); ylabel('Magnitude (dB)');
 
 % Filtering a noise signal
-% noise = rand(1, Fs/2); 
-% noisy_noise = [noise, -noise]; % random generated noise with a mean of 0
-% disp(mean(noisy_noise));
 noise = rand(1, Fs);
 centered_noise = noise - mean(noise);
-
 noise_filter = filter(b, a, noise);
 figure;
-spectrogram(noise_filter);
+spectrogram(noise_filter, 'yaxis');
 
