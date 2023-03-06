372b2f68afce1a9c7b42bf8513fd69a60e6c5bad
ZhannaKlimanova <zklimanova@yahoo.com>
2023-03-06 02:03:39 -0500

    Adding 6 and 7.

--- 7 Bandlimited Oscillator/get_oscillator.m
+++ 7 Bandlimited Oscillator/get_oscillator.m
@@ -3,14 +3,8 @@ dur = 1;
 NT = floor(dur*fs);
 t = dur*(0:NT-1)/NT;
 x = zeros(NT, 1);
-
-% f0s = logspace(1.75, 3.75, 10);
-
-% a = 0.79;
-% for j = 1:length(f0s)
+ 
 for i = 1:NT
     x(i) = sin(2*pi*f0*t(i)) / (1+a^2-2*a*cos(2*pi*f0*t(i)));
 end
  
-
-% end
--- 7 Bandlimited Oscillator/test_oscillator.m
+++ 7 Bandlimited Oscillator/test_oscillator.m
@@ -1,23 +1,17 @@
 f0s = logspace(1.75, 3.75, 10);
 fs = 44100; 
-wave = get_oscillator(0.00001, f0s(10), fs); % when choosing a's the aliasing energy needs to be very small
-wave = get_oscillator(4, f0s(9), fs);
-% figure;
-% plot(wave);
+frequency =f0s(10);
+a = 2.5;
+wave = get_oscillator(a, frequency, fs);
+
 block_wave = wave(1: 2^14);
-% figure;
-% plot(block_wave);
 
 window = hann(2^14);
 windowed_wave = block_wave.*window;
-% figure;
-% plot(windowed_wave);
 fft_wave = abs(fft(windowed_wave));
-% fftLength = floor(length(windowed_wave)) + 1;
 fftLength = length(windowed_wave);
 wave_db = db(fft_wave);
 freq = 0:fs/length(fft_wave):(fs*(1-1/length(fft_wave)));
 
-
 figure;
-plot(freq(1: fftLength), wave_db(1: fftLength)); xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)'); title('Frequency Response');
+plot(freq(1: fftLength), wave_db(1: fftLength)); xlabel(sprintf('Frequency (Hz): %d', frequency)); ylabel('Magnitude (dB)'); title('Frequency Response');
